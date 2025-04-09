127c1e50e18bd27305ed9fc6f6eee929fa70b317
athphane <athphane@gmail.com>
2025-04-09 11:07:09 +0500

    feat: locale_direction helper function

--- src/helpers.php
+++ src/helpers.php
@@ -1,9 +1,10 @@
 <?php
 
 use Illuminate\Support\Arr;
+use Javaabu\Translatable\Facades\Languages;
 use Javaabu\Translatable\Models\Language;
 
-if (! function_exists('appGetLocale')) {
+if (!function_exists('appGetLocale')) {
     /**
      * One function helper to get the current locale.
      *
@@ -15,7 +16,7 @@ if (! function_exists('appGetLocale')) {
     }
 }
 
-if (! function_exists('_d')) {
+if (!function_exists('_d')) {
     /**
      * Translate the given message and use the default locale if locale not specified.
      *
@@ -56,5 +57,22 @@ if (!function_exists('translate_route')) {
         $parameters['language'] = $locale;
 
         return route($name, $parameters, $absolute);
+    }
+}
+
+if (!function_exists('locale_direction')) {
+    /**
+     * Generate a translatable route for the model.
+     *
+     * @param  Language|string|null  $language
+     * @return string
+     */
+    function locale_direction(Language|string $language = null): string
+    {
+        $language = $language instanceof Language
+            ? $language
+            : Languages::get($language ?? app()->getLocale());
+
+        return $language && $language->is_rtl ? 'rtl' : 'ltr';
     }
 }
